0c549f4dd55563dfe4bee47d3c43689b204fae0a
Olivia Schaefer <taygetea@gmail.com>
2016-11-21 15:09:59 -0800

    made it look better i guess

--- antcolony.py
+++ antcolony.py
@@ -1,20 +1,7 @@
 import random
-import pdb
-import pprint
 import time
 from termcolor import cprint, colored
 from collections import defaultdict
-
-
-xbound = 70
-ybound = 70
-nests = ((25,25), (40,20))
-badgoal = (8,3)
-goodgoal = (15,18)
-goals = ((8,3), (15,18), (54, 5))
-goals = [(random.randrange(1,xbound-1), random.randrange(1,ybound-1)) for i in range(2)]
-decayrate = 0.002
-antcount = 5
 
 
 class Ant(object):
@@ -27,16 +14,19 @@ class Ant(object):
         self.wandertime = 5
 
 
-    def getweight(self, offset_x, offset_y):
+    def getweight(self, offset_x, offset_y, world):
+        # Not sure whether I want to fix this or not.
 
-        meanweight = sum((world[self.xpos-1][self.ypos],
-                         world[self.xpos+1][self.ypos],
-                         world[self.xpos][self.ypos+1],
-                         world[self.xpos][self.ypos-1]))/4
+        raise NotImplementedError
 
-        weight = world[self.xpos + offset_x][self.ypos + offset_y]
-        weight = weight - meanweight + (random.random() - 0.5)
-        return [(offset_x, offset_y), weight]
+  #      meanweight = sum((world[self.xpos-1][self.ypos],
+  #                       world[self.xpos+1][self.ypos],
+  #                       world[self.xpos][self.ypos+1],
+  #                       world[self.xpos][self.ypos-1]))/4
+
+  #      weight = world[self.xpos + offset_x][self.ypos + offset_y]
+  #      weight = weight - meanweight + (random.random() - 0.5)
+  #      return [(offset_x, offset_y), weight]
 
 
     def update_history(self):
@@ -45,24 +35,17 @@ class Ant(object):
             self.history = self.history[-50:]
 
 
-    def update_pheromone(self):
-        level = world[self.xpos][self.ypos] + 1
-        if level > 9:
-            level = 9
-        world[self.xpos][self.ypos] = level
-        pheromones[str((self.xpos, self.ypos))] = level
-
 
     def wander(self):
-
-        xnew, ynew = random.choice(((-1,0), (1,0), (0,-1), (0,1)))
+        '''Go to a random nearby square. Except if we were just there.'''
+        xnew, ynew = random.choice(((-1,0), (1,0), (0,-1), (0,1), (-1,-1), (-1,1), (1,1), (1,-1)))
         if (self.xpos + xnew, self.ypos + ynew) == self.history[-1]:
             xnew, ynew = xnew*-1, ynew*-1
-        #pdb.set_trace()
         self.xpos, self.ypos = self.xpos + xnew, self.ypos + ynew
 
 
-    def go_home(self):
+    def go_home(self, nests):
+        '''This follows the path the ant took to the food back to the nest. Not very useful yet.'''
         if (self.xpos, self.ypos) in nests:
             self.carrying_food = False
         try:
@@ -70,12 +53,12 @@ class Ant(object):
         except IndexError:
             self.history = [(self.xpos, self.ypos)]
         self.check_bounds()
-        self.update_pheromone()
 
 
-    def follow_pheromone(self):
+    def follow_pheromone(self, world):
+        '''If on a pheromone tile, '''
         acc = (0, (0,0))
-        for cell in ((-1,0), (1,0), (0,-1), (0,1)):
+        for cell in ((-1,0), (1,0), (0,-1), (0,1), (-1,-1), (-1,1), (1,1), (1,-1)):
             x = cell[0] + self.xpos
             y = cell[1] + self.xpos
             try:
@@ -84,11 +67,12 @@ class Ant(object):
                     self.xpos, self.ypos = acc[1]
             except IndexError:
                 pass
-        if acc[0] == 0:
+        if acc[0] == 0 or acc[0] > 7:
             self.wander()
 
 
-    def check_bounds(self):
+
+    def check_bounds(self, nests):
         if self.xpos >= xbound:
             self.xpos, self.ypos = random.choice(nests)
             self.carrying_food = False
@@ -103,7 +87,7 @@ class Ant(object):
             self.carrying_food = False
 
 
-    def update(self):
+    def update(self, world, nests):
         self.carrying_food = False # bypassing go_home
         if not self.carrying_food:
             if self.wandertime > 0:
@@ -117,17 +101,22 @@ class Ant(object):
                     if world[self.xpos][self.ypos] == 0:
                         self.wander()
                     else:
-                        self.follow_pheromone()
-            self.check_bounds()
-            self.update_pheromone()
-            if (self.xpos, self.ypos) in goals:
-                self.carrying_food = True
+                        self.follow_pheromone(world)
+            self.check_bounds(nests)
             self.update_history()
         else:
-            self.go_home()
+            self.go_home(nests)
 
+def update_pheromone(world, pheromones, xpos, ypos):
+    level = world[xpos][ypos] + 1
+    if level > 9:
+        level = 9
+    world[xpos][ypos] = level
+    pheromones[str((xpos, ypos))] = level
+    return world, pheromones
 
-def drawgrid():
+def move_ants(world, nests, goals, ants):
+
     grid = [''.join([str(round(char)) for char in row]) for row in world]
 
     for nest in nests:
@@ -142,14 +131,17 @@ def drawgrid():
         row = list(grid[ant.xpos])
         row[ant.ypos] = '-'
         grid[ant.xpos] = ''.join(row)
+    return grid
 
+def drawgrid(grid):
+    #print("\n"*120)
     colorgrid = []
     for row in grid:
         crow = ""
         for char in list(row):
             try:
                 if int(char) == 0:
-                    crow += char
+                    crow += ' '
                 elif int(char) == 1:
                     crow += colored(char, 'red')
                 elif int(char) == 2:
@@ -161,19 +153,21 @@ def drawgrid():
             except ValueError:
                 crow += colored(char, 'cyan')
         colorgrid.append(crow)
-    for row in colorgrid:
-        print(row)
+    print("0123456789"*(xbound//10))
+    for idx, row in enumerate(colorgrid):
+        print(row + ' %d' % idx)
+
+xbound = 70
+ybound = 70
+nests = ((25,25), (40,20))
+goals = [(random.randrange(1,xbound-1), random.randrange(1,ybound-1)) for i in range(2)]
+decayrate = 0.002
+antcount = 25
+pheromones = defaultdict(lambda: 0)
+world = [[0 for i in range(xbound)] for j in range(ybound)]
 
 if __name__ == '__main__':
-
-
-    pheromones = defaultdict(lambda: 0)
-
-    world = [[0 for i in range(xbound)] for j in range(ybound)]
-
     ants = [Ant(*random.choice(nests)) for i in range(antcount)]
-
-
     timestep = 0
     while True:
         timestep += 1
@@ -188,15 +182,12 @@ if __name__ == '__main__':
         for pos in pheromones.keys():
             if pheromones[pos] == 0:
                 del pheromones[pos]
-#    for rowidx, row in enumerate(world):
-#        for colidx, col in enumerate(row):
-#            if col > decayrate:
-#                world[rowidx][colidx] = col - decayrate
-#            else:
-#                world[rowidx][colidx] = 0
-        #time.sleep(0.1)
+        time.sleep(0.01)
         for ant in ants:
-            ant.update()
-        if timestep % 20 == 0:
-            drawgrid()
+            ant.update(world, nests)
+        grid = move_ants(world, nests, goals, ants)
+        for ant in ants:
+            world, pheromones = update_pheromone(world,pheromones, ant.xpos, ant.ypos)
+        if timestep % 10 == 0:
+            drawgrid(grid)
 
